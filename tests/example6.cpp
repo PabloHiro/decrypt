2b4ade2fd6cb73dc272786bd8b98cfc7fa5b6b14
PabloHiro <pablo.hiroshi.a.m@gmail.com>
2017-10-30 13:40:13 +0100

    Example 6 solved :D

--- tests/example6.cpp
+++ tests/example6.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 #include <decrypt.hpp>
 #include <transposition.hpp>
@@ -9,6 +10,27 @@ int main ( int argc, char **argv )
 {
     std::string the_text = "GEEIOLLSATAESDDLLAMAILANIOELNSACENARREUSPTUAAEOAPVONICSDEELUNSCSIDUSISNDOEDAMERAODJARNMDIDCDTAEELSMREN";
     std::string the_key_word = "MEDICAMENTOS";
-    std::cout << decrypt<transposition>( the_text, the_key_word ) << std::endl;
+    std::string password;
+    bool found = false;
+    for( size_t pwd_size = 3; pwd_size < the_key_word.size(); ++pwd_size )
+    {
+        if(found) break;
+        password.clear();
+        std::cout << "Trying with password size " << pwd_size << std::endl;
+        for( size_t i = 0; i < pwd_size; ++i )
+        {
+            password.push_back('A' + i);
+        }
+        do {
+            std::string answer = decrypt<transposition>( the_text, password );
+            if ( answer.find( the_key_word ) != std::string::npos )
+            {
+                std::cout << answer << std::endl;
+                std::cout << password << std::endl;
+                found = true;
+                break;
+            }
+        } while( std::next_permutation(password.begin(), password.end()) );
+    }
     return 0;
 }
